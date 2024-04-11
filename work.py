27dcd059da5b5a5f30162830e14bc39575997a35
LUC4SWM <lawoldemichael@gmail.com>
2024-04-10 17:02:39 -0700

    P1 done

--- work.py
+++ work.py
@@ -5,11 +5,22 @@ import scipy as sp
 # Task 1:
 def percent_of_mean(stock_idx):
     """
+    Calculate the percentage of each stock index relative to the mean of indices.
 
+    :param stock_idx: A 1-D array containing the values for daily stock indices.
+    :type stock_idx: numpy.ndarray
 
-    :param stock_idx: A 1-D array with values for daily stock indices.
-    :return: percent_vals: A list of floats representing the percent value for each stock index relative to the mean of indices.
+    :return percent_vals: A list of floats representing the percent value of each stock index relative to the mean of
+    indices.
+    :rtype: list[float]
+
     """
+    # Get the mean of the stock indices.
     mean = np.mean(stock_idx)
+
+    # Calculate the percentage value for each stock index relative to the mean
     percent_vals = (stock_idx / mean) * 100
+
     return percent_vals
+
+
